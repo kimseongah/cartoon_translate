fd610e0b8cea981b06ae852072b2c94e5df47d9d
dhki <kdh101800@gmail.com>
2024-11-25 14:56:43 +0900

    fix: error handling format

--- utils/text_translation/translate.py
+++ utils/text_translation/translate.py
@@ -12,11 +12,11 @@ Translate character scripts and sound effects to Korean using ChatGPT.
 and try to make dictionary with returned gpt answer(string)
 
 Caution:
-- use this function with try/except code.
 - if gpt answer with other string, not only json format,
-  this function can make error
-- check return type
-  (string: error message, dictionary: translated strings)
+  this function can return error
+
+- check error of return value
+  if ret["error"] is True, There are some error in making translation 
 '''
 def translate_to_korean(character_scripts = [], sound_effect = []):
     prompt = make_prompt(character_scripts, sound_effect)
@@ -36,13 +36,11 @@ def translate_to_korean(character_scripts = [], sound_effect = []):
     )
     
     try:
-        ret = json.loads(completion.choices[0].message.content)
-    except json.JSONDecodeError as e:
-        return f"Json decode error (not json format): {e}"
-    except:
-        return f"Unexpected error: {e}"
-
-    return ret
+        response = json.loads(completion.choices[0].message.content)
+        ret = {"error" : False, "data" : response}
+        return ret
+    except Exception as e:
+        ret = {"error" : True, "data" : f"Json decode error (not json format): {e}"} 
 
 def make_prompt(cs = [], se = []):
     default_prompt = "일본 만화의 대사, 효과음을 한국 만화처럼 자연스럽게 번역해서 코드 형식 없이 JSON 형식으로의 문자열만 전달해줘."
